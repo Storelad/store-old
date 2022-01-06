9fbd8bd83a7861fdf237046352838ff905180684
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2022-01-06 13:09:14 +0100

    first commit

---
+++ s.php
@@ -0,0 +1,21 @@
+<?php
+include('path.php');
+AddApp::helpers(['store/path.php']);
+//$_SESSION['store']['about'] = 1;
+if(isset($_GET['store'])){
+    if(isset($_GET['page']) && $_GET['page'] === 'about'){
+        include(ROOT_PATH . '/store/about.php');
+        unset($_SESSION['store']['about']);
+    }elseif(isset($_SESSION['store']['about'])){
+        include(ROOT_PATH . '/store/about.php');
+        unset($_SESSION['store']['about']);
+    }elseif(isset($_SESSION['store']['contact'])){
+        include(ROOT_PATH . '/store/contact.php');
+        unset($_SESSION['store']['contact']);
+    }else{
+        include(ROOT_PATH . '/store/index.php');
+    }
+}else{
+    header('location: ' . BASE_URL . '/');
+    exit();
+}
